b15f2968be21c2b7f8ee6629ee4daf70a73421f0
Lixard <Lixard870@gmail.com>
2020-10-10 18:14:25 +0300

    Первое задание первой лабы

---
+++ 9_lab/src/main/java/ru/student/ArrayListSurnamesSort.java
@@ -0,0 +1,27 @@
+package ru.student;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListSurnamesSort {
+
+    private static final int SURNAMES_LENGTH = 10;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> input = new ArrayList<>(SURNAMES_LENGTH);
+        for (int i = 1; i <= SURNAMES_LENGTH; i++) {
+            System.out.print(i + ". ");
+            input.add(in.nextLine());
+        }
+        in.close();
+
+        System.out.println("--- Sorted ---");
+        input.sort(String::compareToIgnoreCase);
+        input.forEach(System.out::println);
+
+        System.out.println("--- Distinct ---");
+        input.stream().distinct().forEachOrdered(System.out::println);
+
+    }
+}
