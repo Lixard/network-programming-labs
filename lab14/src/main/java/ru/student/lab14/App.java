d085d2088503955e03059766bb472f97a6e77270
Lixard <Lixard870@gmail.com>
2021-01-10 18:33:11 +0300

    Lab 14 fixes

--- lab14/src/main/java/ru/student/lab14/App.java
+++ lab14/src/main/java/ru/student/lab14/App.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EntityScan
-@EnableWebMvc
 public class App extends SpringBootServletInitializer {
     public static void main(String[] args) {
         final var springApplication = new SpringApplication(App.class);
